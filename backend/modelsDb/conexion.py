aeec864c4d76e0e913d2b209d316b2a833e0e906
gpanelli3 <gpanelli3@gmail.com>
2025-04-22 15:52:33 -0300

    conc

--- backend/modelsDb/conexion.py
+++ backend/modelsDb/conexion.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base=declarative_base()
-engine= create_engine('mysql+pymysql://usuario:root@database:3306/stockcommerce')
+engine= create_engine('mysql+pymysql://genarodesarrollo:password@localhost:3306/stockControl')
 Session=sessionmaker(bind=engine)
 session=Session()
 
-#la idea seria usar por ahora la base de datos en el workbench para avanzar en el proyecto.
+#la idea seria usar por ahora la base de datos en el workbench para avanzar en el proyecto. (working)
 
 #debo cambiar la conexion al workbench, ya que esta con docker compose.
 
