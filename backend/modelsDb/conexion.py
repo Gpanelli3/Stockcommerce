6c4a33fa99c816a11822ed2135c662bb68fada1e
gpanelli3 <gpanelli3@gmail.com>
2025-04-22 12:50:35 -0300

    database-ready

--- backend/modelsDb/conexion.py
+++ backend/modelsDb/conexion.py
@@ -6,3 +6,13 @@ Base=declarative_base()
 engine= create_engine('mysql+pymysql://usuario:root@database:3306/stockcommerce')
 Session=sessionmaker(bind=engine)
 session=Session()
+
+#la idea seria usar por ahora la base de datos en el workbench para avanzar en el proyecto.
+
+#debo cambiar la conexion al workbench, ya que esta con docker compose.
+
+
+# INGRESO PRODUCTOS
+# GENERAR VENTAS
+# INGRESO DE CLIENTES
+# INCORPORAR API DE MP
