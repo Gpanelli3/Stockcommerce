b3bf5f678a8cec6f2356e41c803e3a86a4f01264
gpanelli3 <gpanelli3@gmail.com>
2025-05-25 11:07:42 -0300

    arreglo db

--- backend/modelsDb/conexion.py
+++ backend/modelsDb/conexion.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base=declarative_base()
-engine= create_engine('mysql+pymysql://genarodesarrollo:password@127.0.0.1:3306/stockControl')
+engine = create_engine(
+    'mysql+pymysql://genarodesarrollo:password@127.0.0.1:3306/stockControl',
+    connect_args={"connect_timeout": 10}
+)
 Session=sessionmaker(bind=engine)
 session=Session()
 
